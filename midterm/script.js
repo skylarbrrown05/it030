dedd545483e6a116149c8c0548d95137f2aa5835
Skylar Brown <skylarbrrown@gmail.com>
2025-04-03 16:29:37 -0400

    script

--- midterm/script.js
+++ midterm/script.js
@@ -1,18 +1,7 @@
 // Display a message in the browser console
 console.log("CSS + JavaScript is powerful!");
 
-// Show an alert message when the page loads
-//window.onload = function () {
-    //alert("Welcome to my webpage!");
-//};
 
-// Function to toggle between light mode and dark mode
-//function changeTheme() {
-   // document.body.classList.toggle("dark-mode");
-//}
-
-
- // color:black;
 const colors = ["rgb(180, 176, 176)","rgb(233, 195, 201)","rgb(35, 31, 32)","rgb(192, 193, 213)"]; //grey, red, black, blue
 const userName = prompt("What's your name?"); 
 const colorChoice = parseInt(prompt("Pick a background color: 0 for grey, 1 for red, 2 for black, 3 for blue"));
