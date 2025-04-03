c1a5b383a314e3d049bc30c05672b746552ea1ca
Skylar Brown <skylarbrrown@gmail.com>
2025-04-03 03:18:20 -0400

    testing background change

--- assignment6/script.js
+++ assignment6/script.js
@@ -13,8 +13,8 @@ function changeTheme() {
 
 
 const colors = ["#fcf7fc ","#f0f9fd ","#f6f0fd ","#f0f4fd "]; //light pink, greem, purple, blue
-//const userName = prompt("What's your name?"); 
-const colorChoice = prompt("Pick a background color: 0 for pink, 1 for green, 2 for purple, 3 for blue");
+const userName = prompt("What's your name?"); 
+const colorChoice = parseInt(prompt("Pick a background color: 0 for pink, 1 for green, 2 for purple, 3 for blue"));
 
 console.log("The first color in the array is:", colors[0]);
 
