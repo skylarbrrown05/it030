506920035406e209e0cd65ede0e88b8a089b16bc
Skylar Brown <skylarbrrown@gmail.com>
2025-04-03 17:05:32 -0400

    color change java

--- midterm/script.js
+++ midterm/script.js
@@ -1,8 +1,8 @@
 
 
-const colors = ["rgb(180, 176, 176)","rgb(233, 195, 201)","rgb(35, 31, 32)","rgb(192, 193, 213)"]; //grey, red, black, blue
+const colors = ["rgb(180, 176, 176)","rgb(233, 195, 201)","rgb(229, 228, 199)","rgb(192, 193, 213)"]; //grey, red, beige, blue
 const userName = prompt("Whats your name?"); 
-const colorChoice = parseInt(prompt("Pick a background color: 0 for grey, 1 for red, 2 for black, 3 for blue"));
+const colorChoice = parseInt(prompt("Pick a background color: 0 for grey, 1 for red, 2 for beige, 3 for blue"));
 
 // Display a message in the browser console
 console.log("The first color in the array is:", colors[0]);
