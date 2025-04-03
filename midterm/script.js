5392683118a3292bc06e74609cf41049dbb94344
Skylar Brown <skylarbrrown@gmail.com>
2025-04-03 17:12:01 -0400

    color

--- midterm/script.js
+++ midterm/script.js
@@ -1,8 +1,8 @@
 
 
-const colors = ["rgb(180, 176, 176)","rgb(233, 195, 201)","rgb(153, 152, 129)","rgb(192, 193, 213)"]; //grey, red, beige, blue
+const colors = ["rgb(180, 176, 176)","rgb(233, 195, 201)","rgb(52, 51, 51)","rgb(192, 193, 213)"]; //grey, red, black, blue
 const userName = prompt("Whats your name?"); 
-const colorChoice = parseInt(prompt("Pick a background color: 0 for grey, 1 for red, 2 for beige, 3 for blue"));
+const colorChoice = parseInt(prompt("Pick a background color: 0 for grey, 1 for red, 2 for black, 3 for blue"));
 
 // Display a message in the browser console
 console.log("The first color in the array is:", colors[0]);
