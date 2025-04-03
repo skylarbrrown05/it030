af22cd6102b664fa915677ac4da74a32e251578b
Skylar Brown <skylarbrrown@gmail.com>
2025-04-02 23:32:42 -0400

    update console log

--- assignment6/script.js
+++ assignment6/script.js
@@ -15,6 +15,8 @@ const colors = ["#fcf7fc ","#f0f9fd ","#f6f0fd ","#f0f4fd "]; //light pink, gree
 const userName = prompt("What's your name?"); 
 const colorChoice = prompt("Pick a background color: 0 for pink, 1 for green, 2 for purple, 3 for blue");
 
+console.log("The first color in the array is:", colors[0]);
+
 const userInfo = {
     name: userName,
     selectedColor: colors[colorChoice]
